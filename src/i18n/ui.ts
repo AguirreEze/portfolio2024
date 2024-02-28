568022961c6e01a26eea386a70213f31882cca96
AguirreEze <aguirre.eze95@gmail.com>
2024-02-28 10:30:39 -0300

    feat: i18n for english and spanish

---
+++ src/i18n/ui.ts
@@ -0,0 +1,100 @@
+export const languages = {
+    en: 'English',
+    es: 'Español',
+  };
+  
+  export const defaultLang = 'en';
+  
+  export const ui = {
+    en: {
+        'profesion': 'Front-end Developer.',
+        'linkedin': 'LinkedIn',
+
+        'nav.experience': 'Experience',
+        'nav.proyects': 'Projects',
+        'nav.aboutMe': 'About Me',
+        'nav.contact': 'Contact',
+
+        'hero.openforwork': 'Open to work',
+        'hero.title': "Hey, i'm Ezequiel",
+        'hero.description': '2 years of experience, from Buenos Aires, Argentina. Specialized in the use and optimization of the NextJS / ReactJS framework.',
+        'hero.contact': 'Contact Me',
+
+        'exp[0].title': 'Freelance',
+        'exp[0].date': 'September 2021 - July 2022',
+        'exp[0].company': 'Self-employed',
+        'exp[0].description': 'Working on small projects to improve my knowledge of web development. Developer and maintainer of the Fullstack ReventandoOtrosMundos project',
+
+        'exp[1].title': 'Front-end Developer',
+        'exp[1].date': 'June 2022 - October 2023',
+        'exp[1].company': 'MContigo',
+        'exp[1].description': 'My first job as a Front-end Developer, in which I contributed to various projects of the company, ranging from internal tools to a project with millions of daily visits and published in over ten languages. Unfortunately, the team I belonged to was disbanded.',
+
+        'project.code' : "Code",
+        'project.production' : "Production",
+
+        'projects[0].title': "Reventando Otros Mundos",
+        'projects[0].description': "Library used in the Invernalia radio program to list the games already broadcasted on Twitch and the animes reviewed during the program.",
+        'projects[0].link': "https://reventandootrosmundos.vercel.app/",
+        'projects[0].image': "/projects/ReventandoOtrosMundos.webp",
+        'projects[0].imgAlt': "Project sneak peek image",
+
+        'projects[1].title': "Step To Health",
+        'projects[1].description': "Belonging to MContigo, which features health-related articles. In this project, I suggested and implemented On-Demand Static Revalidation, resulting in a significant reduction in server costs. Additionally, I contributed to the implementation of new designs and Amazon components, significantly improving Core Web Vitals results.",
+        'projects[1].link': 'https://steptohealth.com',
+        'projects[1].image': "/projects/StepToHealth.webp",
+        'projects[1].imgAlt': "Project sneak peek image",
+        
+        'projects[2].title': "Travel Plannet",
+        'projects[2].description': "Website owned by MContigo, where I performed a complete migration from ReactJS to NextJS v13. Additionally, I implemented a deployment pipeline and the corresponding Docker configuration.",
+        'projects[2].link': 'https://travelplannet.com',
+        'projects[2].image': "/projects/TravelPlannet.webp",
+        'projects[2].imgAlt': "Project sneak peek image",
+
+        'aboutMe.texts[0]': 'My name is Ezequiel Aguirre. I started programming in early 2021. In mid-September of the same year, I decided to focus on learning web development.',
+        'aboutMe.texts[1]': 'I completed the free bootcamp at fullstackopen, where I learned the basics of ReactJS. From there, I decided to learn the Next.js framework, focusing on understanding different types of implementations without relying on additional third-party libraries.',
+        'aboutMe.texts[2]': 'Thanks to this, I was able to secure my first job as a Front-end Developer at MContigo. In this role, I contributed to various projects within the company, including internal use tools.',
+        'aboutMe.texts[3]': 'Currently, I am seeking my next employment opportunity while staying updated on the new features of ReactJS/NextJS and learning to use Astro.',
+
+        'footer.disclaimer': 'Aguirre Ezequel. (Design based on portfolio made by'
+    },
+    es: {
+        'profesion': 'Desarrollador Front-end.',
+
+        'nav.experience': 'Experiencia',
+        'nav.proyects': 'Proyectos',
+        'nav.aboutMe': 'Sobre mí',
+        'nav.contact': 'Contacto',
+
+        'hero.title': "Hey, soy Ezequiel",
+        'hero.openforwork': 'Disponible para trabajar',
+        'hero.description': '2 años de experiencia, de Buenos Aires, Argentina. Especializado en el uso y optimización del framework NextJS / ReactJS.',
+        'hero.contact': 'Contáctame',
+
+        'exp.date[0]': 'Septiembre 2021 - Julio 2022',
+        'exp.title[0]': 'Freelance',
+        'exp.description[0]': 'Trabajando en pequeños para mejorar mis conocimientos sobre el desarrollo web. Desarrollador y mantenedor del proyecto Fullstack ReventandoOtrosMundos',
+
+        'exp.date[1]': 'Junio 2022 - Octubre 2023',
+        'exp.description[1]': 'Mi primer trabajo como Desarrollador Front-end, en el cual contribuí en varios proyectos de la empresa, desde herramientas de uso interno hasta un proyecto con millones de visitas diarias y publicado en más de diez idiomas. Lamentablemente, el equipo al cual pertenecía fue desarmado.',
+
+        'project.code' : "Código",
+        'project.production' : "Producción",
+
+        'projects[0].description': "Biblioteca utilizada en el programa de radio Invernalia para enlistar los juegos ya transmitidos en Twitch y los Animes reseñados durante el programa.",
+
+        'projects[1].title': "Mejor con Salud",
+        'projects[1].description': "Perteneciente a MContigo que presenta artículos relacionados con la salud. En este proyecto, sugerí e implementé On-Demand Static Revalidation, lo que resultó en una reducción considerable en los costos del servidor. Además, contribuí con la implementación de nuevos diseños y componentes de Amazon, y ayudé significativamente a mejorar los resultados de Core Web Vitals.",
+        'projects[1].link': 'https://mejorconsalud.as.com',
+        'projects[1].image': "/projects/MejorConSalud.webp",
+
+        'projects[2].description': "Sitio web que pertenece a MContigo, donde realicé una migración completa de la base de ReactJS a NextJS v13. Además, implementé un pipeline de despliegue y la correspondiente configuración de Docker.",
+
+        'aboutMe.texts[0]': 'Me llamo Ezequiel Aguirre. Comencé en la programación a principios del año 2021. A mediados de septiembre del mismo año, decidíenfocarme en aprender desarrollo web.',
+        'aboutMe.texts[1]': 'Completé el bootcamp gratuito en fullstackopen, donde aprendí lo básico de ReactJS. A partir de esto, decidí aprender el Framework NextJS, concentrándome en comprender diferentes tipos de implementaciones sin utilizar más librerías de terceros.',
+        'aboutMe.texts[2]': 'Gracias a esto, pude conseguir mi primer trabajo como Desarrollador Front-end en la empresa MContigo. En este puesto, pude contribuir en distintos proyectos pertenecientes a la empresa, incluyendo herramientas de uso interno.',
+        'aboutMe.texts[3]': 'Actualmente me encuentro en busca de mi próximo empleo, mientras me mantengo al tanto de las nuevas características de ReactJS/NextJS y aprendiendo a utilizar Astro.',
+
+        'footer.disclaimer': 'Aguirre Ezequel. (Diseño basado en portfolio armado por'
+      },
+  } as const;
